802738cbab2d245b325ca24ec4dae2be3c3e8ac9
Dawid Król <dawidkrol7@gmail.com>
2024-11-30 21:29:55 +0100

    Basic functionality (#5)

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application } from 'express';
+import router from './routes/router';
+import errorHandling from './library/utils/errorHandler';
+
+const app: Application = express();
+
+app.use(express.json({ limit: '10kb' }));
+
+app.use('/api', router);
+
+app.use(errorHandling);
+
+export default app;
